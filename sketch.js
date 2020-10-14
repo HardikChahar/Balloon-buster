29c1676ee91f5fe2adca025385a2da2f55deb435
HardikChahar <72494677+HardikChahar@users.noreply.github.com>
2020-10-14 15:12:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,95 +1,32 @@
-var background;
-var testingBackground;
-var blueBalloon,blueBalloonImg;
-var greenBalloon,greenBalloonImg;
-var redBalloon,redBalloonImg; 
-var pinkBalloon,pinkBalloonImg;
-var bow,bowImage;
-var arrow,arrowImg; 
-var num;
+var fixedRect;
+var movingRect;
 
-
-function preload(){
-  testingBackground=loadImage("background0.png");
-  
-  blueBalloonImg = loadImage("blue_balloon0.png");
-  greenBalloonImg = loadImage("green_balloon0.png");
-  redBalloonImg = loadImage("red_balloon0.png");
-  bowImage=loadImage("bow0.png"); 
-  arrowImg=loadImage("arrow0.png");
-  pinkBalloonImg=loadImage("pink_balloon0.png")
-}
 
 function setup() {
-  background("red");
-  background = createSprite(200,200,30,30);
-  background.addImage("a",testingBackground);
-  
-background.x = background.width /1.95;
- 
-  
-  bow = createSprite(350,mouseY,10,100)
-  bow.addImage("a",bowImage);
-  
-  //add code here
-   
-  background.velocityX=-4;
-  background.scale=3;
-
-  
-arrow=createSprite(360,bow.y,10,10);
-arrow.velocityX=-8;   
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+  movingRect=createSprite(200,200,50,40);
+  fixedRect=createSprite(300,200,50,20);
+  movingRect.shapeColor="green";
+  fixedRect.shapeColor="green";
 }
 
 function draw() {
-  //add code here
-  if (background.x < 0) {
-  background.x = background.width / 1.9;
-}bow.y=mouseY;
+  background(255,255,255);
+  movingRect.x=mouseX;
+  movingRect.y=mouseY; 
+  console.log(movingRect.x-fixedRect.x) 
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&&
+    fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&
+    movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2&&
+    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
 
   drawSprites();
-  
-
-  if(keyDown("space")&&arrow.x<200){
-  bow.y=mouseY;
-    arrow=createSprite(360,bow.y,10,10);
-  arrow.addImage("a",arrowImg); 
-    arrow.scale=0.35;
-    arrow.velocityX=-8;
-    arrow.lifetime=30;
-
-}
-
- if(frameCount%40==0){
-   num=Math.round(random(1,4));
-   switch(num){
-     case 1:blueBalloon=createSprite(Math.round(random(100,200)),390,10,10);
-   blueBalloon.addImage("a",blueBalloonImg);
-   blueBalloon.scale=0.1;
-   blueBalloon.velocityY=-4;
-   blueBalloon.lifetime=100;
-       break;
-           case 2:redBalloon=createSprite(Math.round(random(100,200)),390,10,10);
-   redBalloon.addImage("a",redBalloonImg);
-   redBalloon.scale=0.1;
-   redBalloon.velocityY=-4;
-   redBalloon.lifetime=100;
-       break;   
-       case 3:greenBalloon=createSprite(Math.round(random(100,200)),390,10,10);
-   greenBalloon.addImage("a",greenBalloonImg);
-   greenBalloon.scale=0.1;
-   greenBalloon.velocityY=-4;
-   greenBalloon.lifetime=100;
-       break;
-              case 4:pinkBalloon=createSprite(Math.round(random(100,200)),390,10,10);
-   pinkBalloon.addImage("a",pinkBalloonImg);
-   pinkBalloon.scale=1.4;
-   pinkBalloon.velocityY=-4;
-   pinkBalloon.lifetime=100;
-       break;
-       
-   }
-
-  }
- 
 }
